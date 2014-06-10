d93084a56070670a8359ba53ab7da36faf51815e
Juan Carlos Fernandez <jcfernandez@redoop.org>
2014-06-10 18:12:19 +0200

    added kafka check using kafka rpm

---
+++ check_kafka.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# vim: ts=4:sw=4:et:sts=4:ai:tw=80
+from utils import StringContext,krb_wrapper
+import nagiosplugin
+import argparse
+import subprocess
+
+def parser():
+    version="0.1"
+    parser = argparse.ArgumentParser(description="Check zookeeper znodes existence and content")
+    parser.add_argument('-H','--hosts',action='store',required=True)
+    parser.add_argument('-K','--kafka_list_bin',action='store',default='/usr/lib/kafka/bin/kafka-list-topic.sh')
+    parser.add_argument('-T','--topic',action='store')
+    args = parser.parse_args()
+    return args
+
+class KafkaTopics(nagiosplugin.Resource):
+    def parse_topics(self,test):
+        response = subprocess.Popen([self.kafka_list_bin,'--zookeeper', self.zkserver,test],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output,err = response.communicate()
+        return output,err
+
+    def __init__(self,args):
+        self.zkserver = args.hosts
+        self.kafka_list_bin = args.kafka_list_bin
+        self.get_status()
+
+    def get_status(self):
+        self.under_replicated=self.parse_topics('--under-replicated-partitions')[0]
+        self.unavailable=self.parse_topics('--unavailable-partitions')[0]
+
+    def probe(self):
+        yield nagiosplugin.Metric('under_replicated',self.under_replicated if self.under_replicated != "" else 'empty',context="empty")
+        yield nagiosplugin.Metric('unavailable',self.unavailable if self.unavailable !="" else 'empty',context="empty")
+        
+@nagiosplugin.guarded
+def main():
+    timeout=10 # default
+    args = parser()
+    check = nagiosplugin.Check(KafkaTopics(args),
+        StringContext('empty','empty'))
+    check.main(timeout=timeout)
+    if args.secure: auth_token.destroy()
+
+if __name__ == '__main__':
+    main()
